da560f2cc46812e4367e32d646d8a871da65b52f
Samirabdulllah <samira.abdullah@upr.edu>
2025-09-09 14:57:48 +0000

    arreglar espacios

--- main.cpp
+++ main.cpp
@@ -18,34 +18,39 @@ int main (){
     cout << "Números aleatorios generados: " << numeroAleatorio1 << " " << numeroAleatorio2 << " "<<numeroAleatorio3 << endl;
 
     //Ordenar los números aleatorios
-    cout << "Orden descendiente: "
+    cout << "Orden descendiente: ";
+
+    // En el caso de que el numero mayor sea el primer aleatorio
     if ( numeroAleatorio1 >= numeroAleatorio2 && numeroAleatorio1 >= numeroAleatorio3)
     {
         cout << numeroAleatorio1<< " >= " ;
         if (numeroAleatorio2 >= numeroAleatorio3)
-            cout << numeroAleatorio2 << " >= " << numeroAleatorio3;
+            cout << numeroAleatorio2 << " >= " << numeroAleatorio3 << endl;
         else 
-            cout << numeroAleatorio3 << " >= " << numeroAleatorio2;
+            cout << numeroAleatorio3 << " >= " << numeroAleatorio2 << endl;
     }
+
+    // En el caso de que el numero mayor sea el segundo aleatorio
 
     if ( numeroAleatorio2 >= numeroAleatorio1 && numeroAleatorio2 >= numeroAleatorio3)
     {
         cout << numeroAleatorio2 << " >= " ;
         if (numeroAleatorio1 >= numeroAleatorio3)
-            cout << numeroAleatorio1 << " >= " << numeroAleatorio3;
+            cout << numeroAleatorio1 << " >= " << numeroAleatorio3 << endl;
         else 
-            cout << numeroAleatorio3 << " >= " << numeroAleatorio1;
+            cout << numeroAleatorio3 << " >= " << numeroAleatorio1 << endl;
     }
+
+    // En el caso de que el numero mayor sea el tercero aleatorio
 
     if ( numeroAleatorio3 >= numeroAleatorio2 && numeroAleatorio3 >= numeroAleatorio1)
     {
         cout << numeroAleatorio3 << " >= " ;
         if (numeroAleatorio1 >= numeroAleatorio2)
-            cout << numeroAleatorio1 << " >= " << numeroAleatorio2;
+            cout << numeroAleatorio1 << " >= " << numeroAleatorio2 << endl;
         else 
-            cout << numeroAleatorio2 << " >= " << numeroAleatorio1;
+            cout << numeroAleatorio2 << " >= " << numeroAleatorio1 << endl;
     }
-
 
     return 0;
 }
