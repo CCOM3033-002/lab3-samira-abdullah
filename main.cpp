48c1a8e2eb194b7b6f04afaf48ef8a76f7568b96
Samirabdulllah <samira.abdullah@upr.edu>
2025-09-09 14:52:43 +0000

    primer intento a ordenar las aleatorias

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /* Samira Abdullah
 9 de septiembre de 2025
 Laboratorio 3 
-Colaboradores: */
+Colaboradores: Lillian González */
 
 #include <iostream>
 #include <cstdlib>
@@ -15,10 +15,36 @@ int main (){
     int numeroAleatorio3 = rand() % (100 - 0 + 1) + 0;
     // Desplegar los numeros aleatorios
 
-    cout << "Números aleatorios generados: " << numeroAleatorio1 << numeroAleatorio2 << numeroAleatorio3 << endl;
+    cout << "Números aleatorios generados: " << numeroAleatorio1 << " " << numeroAleatorio2 << " "<<numeroAleatorio3 << endl;
 
     //Ordenar los números aleatorios
-    
+    cout << "Orden descendiente: "
+    if ( numeroAleatorio1 >= numeroAleatorio2 && numeroAleatorio1 >= numeroAleatorio3)
+    {
+        cout << numeroAleatorio1<< " >= " ;
+        if (numeroAleatorio2 >= numeroAleatorio3)
+            cout << numeroAleatorio2 << " >= " << numeroAleatorio3;
+        else 
+            cout << numeroAleatorio3 << " >= " << numeroAleatorio2;
+    }
+
+    if ( numeroAleatorio2 >= numeroAleatorio1 && numeroAleatorio2 >= numeroAleatorio3)
+    {
+        cout << numeroAleatorio2 << " >= " ;
+        if (numeroAleatorio1 >= numeroAleatorio3)
+            cout << numeroAleatorio1 << " >= " << numeroAleatorio3;
+        else 
+            cout << numeroAleatorio3 << " >= " << numeroAleatorio1;
+    }
+
+    if ( numeroAleatorio3 >= numeroAleatorio2 && numeroAleatorio3 >= numeroAleatorio1)
+    {
+        cout << numeroAleatorio3 << " >= " ;
+        if (numeroAleatorio1 >= numeroAleatorio2)
+            cout << numeroAleatorio1 << " >= " << numeroAleatorio2;
+        else 
+            cout << numeroAleatorio2 << " >= " << numeroAleatorio1;
+    }
 
 
     return 0;
