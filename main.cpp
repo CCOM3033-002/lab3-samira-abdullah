45dea39ebb4f80651c1787ee75a4f1545f277dfe
Samirabdulllah <samira.abdullah@upr.edu>
2025-09-09 14:22:53 +0000

    inicializar el generador de numero aleatorios con el numero maximo y minimo

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+main (){
+
+    //Inicializa el Generador de Números Aleatorios con el número máximo y minimo
+    srand(time(0));
+    int numeroAleatorio = rand() % (100 - 0 + 1) + 0;
+
+
+}
 
