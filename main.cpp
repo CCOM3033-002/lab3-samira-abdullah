7d16f645ea9865c7c15c3a3b61f87cc89834edd0
Samirabdulllah <samira.abdullah@upr.edu>
2025-09-09 15:05:03 +0000

    arreglando astetica

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /* Samira Abdullah
 9 de septiembre de 2025
-Laboratorio 3 
+Laboratorio 3: Números Aleatorios
 Colaboradores: Lillian González */
 
 #include <iostream>
@@ -8,19 +8,25 @@ Colaboradores: Lillian González */
 using namespace std;
 int main (){
 
-    //Inicializa el Generador de Números Aleatorios con el número máximo y minimo
+    // Desplegar proposito del programa
+
+    cout << "Este programa desplega tres números aleatorios y los organiza en orden descendiente. " << endl;
+
+    //Inicializando el Generador de Números Aleatorios con el número máximo y minimo deseado de 0-100
+
     srand(time(0));
     int numeroAleatorio1 = rand() % (100 - 0 + 1) + 0;
     int numeroAleatorio2 = rand() % (100 - 0 + 1) + 0;
     int numeroAleatorio3 = rand() % (100 - 0 + 1) + 0;
-    // Desplegar los numeros aleatorios
 
+    // Desplegar los numeros aleatorios en la pantalla
     cout << "Números aleatorios generados: " << numeroAleatorio1 << " " << numeroAleatorio2 << " "<<numeroAleatorio3 << endl;
 
     //Ordenar los números aleatorios
     cout << "Orden descendiente: ";
 
-    // En el caso de que el numero mayor sea el primer aleatorio
+    // En el caso de que el primer numero aleatorio sea el mayor
+
     if ( numeroAleatorio1 >= numeroAleatorio2 && numeroAleatorio1 >= numeroAleatorio3)
     {
         cout << numeroAleatorio1<< " >= " ;
@@ -30,7 +36,7 @@ int main (){
             cout << numeroAleatorio3 << " >= " << numeroAleatorio2 << endl;
     }
 
-    // En el caso de que el numero mayor sea el segundo aleatorio
+    // En el caso de que el segundo numero aleatorio sea el mayor
 
     if ( numeroAleatorio2 >= numeroAleatorio1 && numeroAleatorio2 >= numeroAleatorio3)
     {
@@ -41,7 +47,7 @@ int main (){
             cout << numeroAleatorio3 << " >= " << numeroAleatorio1 << endl;
     }
 
-    // En el caso de que el numero mayor sea el tercero aleatorio
+    // En el caso de que el tercer numero aleatorio sea el mayor
 
     if ( numeroAleatorio3 >= numeroAleatorio2 && numeroAleatorio3 >= numeroAleatorio1)
     {
