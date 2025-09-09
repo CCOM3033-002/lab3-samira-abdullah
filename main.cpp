8a329a3cd073f2b1b567c6b73128cfa163098c3b
Samirabdulllah <samira.abdullah@upr.edu>
2025-09-09 15:08:45 +0000

    versión final del programa

--- main.cpp
+++ main.cpp
@@ -1,31 +1,33 @@
-/* Samira Abdullah
+/* 
+Samira Abdullah
 9 de septiembre de 2025
 Laboratorio 3: Números Aleatorios
-Colaboradores: Lillian González */
+Colaboradores: Lillian González
+ */
 
 #include <iostream>
 #include <cstdlib>
 using namespace std;
 int main (){
 
-    // Desplegar proposito del programa
+    // Desplegar propósito del programa
 
     cout << "Este programa desplega tres números aleatorios y los organiza en orden descendiente. " << endl;
 
-    //Inicializando el Generador de Números Aleatorios con el número máximo y minimo deseado de 0-100
+    //Inicializando el generador de números aleatorios, con el número máximo y minimo deseado de 0-100
 
     srand(time(0));
     int numeroAleatorio1 = rand() % (100 - 0 + 1) + 0;
     int numeroAleatorio2 = rand() % (100 - 0 + 1) + 0;
     int numeroAleatorio3 = rand() % (100 - 0 + 1) + 0;
 
-    // Desplegar los numeros aleatorios en la pantalla
+    // Desplegar los números aleatorios en la pantalla
     cout << "Números aleatorios generados: " << numeroAleatorio1 << " " << numeroAleatorio2 << " "<<numeroAleatorio3 << endl;
 
     //Ordenar los números aleatorios
     cout << "Orden descendiente: ";
 
-    // En el caso de que el primer numero aleatorio sea el mayor
+    // En el caso de que el primer número aleatorio sea el mayor (desplegando los resultados en orden descendiente)
 
     if ( numeroAleatorio1 >= numeroAleatorio2 && numeroAleatorio1 >= numeroAleatorio3)
     {
@@ -36,7 +38,7 @@ int main (){
             cout << numeroAleatorio3 << " >= " << numeroAleatorio2 << endl;
     }
 
-    // En el caso de que el segundo numero aleatorio sea el mayor
+    // En el caso de que el segundo número aleatorio sea el mayor (desplegando los resultados en orden descendiente)
 
     if ( numeroAleatorio2 >= numeroAleatorio1 && numeroAleatorio2 >= numeroAleatorio3)
     {
@@ -47,7 +49,7 @@ int main (){
             cout << numeroAleatorio3 << " >= " << numeroAleatorio1 << endl;
     }
 
-    // En el caso de que el tercer numero aleatorio sea el mayor
+    // En el caso de que el tercer número aleatorio sea el mayor (desplegando los resultados en orden descendiente)
 
     if ( numeroAleatorio3 >= numeroAleatorio2 && numeroAleatorio3 >= numeroAleatorio1)
     {
